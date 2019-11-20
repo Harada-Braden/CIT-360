993d98d7ca4e58601c45566f43af6a429a14bddc
Braden Harada <27602608+Harada-Braden@users.noreply.github.com>
2019-11-20 15:19:36 -0700

    Add files via upload

---
+++ Collections/src/collections/Collections.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+/**
+ *
+ * @author lil_n
+ */
+public class Collections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> dinosaurs = new ArrayList();
+        
+        dinosaurs.add(Eoraptor);
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        dinosaurs.add();
+        
+    }
+
+    private static class List<T> {
+
+        public List() {
+        }
+    }
+
+    private static class ArrayList {
+
+        public ArrayList() {
+        }
+    }
+    
+}
